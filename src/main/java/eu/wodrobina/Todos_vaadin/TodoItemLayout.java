0792dc8d220e82862eadebfe63a464fbfb4e4e33
wowik <ghtany9lat>
2017-09-16 19:47:11 +0200

    next

--- src/main/java/eu/wodrobina/Todos_vaadin/TodoItemLayout.java
+++ src/main/java/eu/wodrobina/Todos_vaadin/TodoItemLayout.java
@@ -1,8 +1,11 @@
 package eu.wodrobina.Todos_vaadin;
 
+import com.vaadin.data.Binder;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.themes.ValoTheme;
 
 public class TodoItemLayout extends HorizontalLayout{
 
@@ -13,6 +16,14 @@ public class TodoItemLayout extends HorizontalLayout{
         done = new CheckBox();
         text = new TextField();
 
-        addComponents(done, text);
+        text.addStyleName(ValoTheme.TEXTFIELD_BORDERLESS);
+
+        addComponent(done);
+        addComponentsAndExpand(text);
+        setDefaultComponentAlignment(Alignment.MIDDLE_LEFT);
+
+        Binder<Todo> binder = new Binder<>(Todo.class);
+        binder.bindInstanceFields(this);
+        binder.setBean(todo);
     }
 }
